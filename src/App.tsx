fb6c6d84f0f41b5463b079aa3173a75a476349d6
Caique Giovannini <caique_giovannini@hotmail.com>
2022-07-12 23:24:45 -0300

    Include routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
-      <h2>Ignite Timer</h2>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
