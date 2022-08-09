8b5e97a7c29b42d9ce955f0474becc02317cfa29
Caique Giovannini <caique_giovannini@hotmail.com>
2022-08-09 00:02:06 -0300

    Define form default values

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,8 +20,17 @@ const newCycleFormValidationSchema = zod.object({
     .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
 
   function handleCreateNewCycle(data) {}
 
